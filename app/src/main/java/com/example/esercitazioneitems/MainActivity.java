2682f3a17078a8692bff03d8e5d3f4c917922933
EnricoLGM <enricolegumi44@gmail.com>
2021-12-10 15:53:01 +0100

    Aggiunta ListView e Bottone con OnClick

--- app/src/main/java/com/example/esercitazioneitems/MainActivity.java
+++ app/src/main/java/com/example/esercitazioneitems/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.esercitazioneitems;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String film[]={"Spiderman", "Titanic", "Balle spaziali"};
+    ListView listaFilm;
+    Button bottoneEsci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaFilm=(ListView)findViewById(R.id.listaFilm);
+        bottoneEsci=(Button)findViewById(R.id.bottoneEsci);
+        ArrayAdapter<String> adapterFilm=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, film);
+        listaFilm.setAdapter(adapterFilm);
+        bottoneEsci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Stai per uscire", Toast.LENGTH_LONG).show();
+            }
+        });
+        //onitamclicklistner da utilizzare sulla lista
+        //listaFilm.setOnItemClickListener(new View.OnClickListener());
     }
 }
